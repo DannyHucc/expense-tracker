eec065a78152a1c50a50ee8c3e29a0a5c2f2c3f8
DannyHucc <dannyhu10011101@gmail.com>
2023-06-28 19:04:13 +0800

    project init

---
+++ app.js
@@ -0,0 +1,37 @@
+// environment
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+// packages and variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const hbshelpers = require('handlebars-helpers')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+const routes = require('routes-file')
+const app = express()
+const helpers = hbshelpers()
+const PORT = process.env.PORT || 8080
+
+// template engine: express-handlebars
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    helpers: helpers
+}))
+app.set('view engine', 'hbs')
+
+// middleware: express-static, body-parser, method-override
+// app.use(express.static('public'))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// middleware: routes
+app.use(routes)
+
+// start the server
+app.listen(PORT, () => {
+    console.log(`Express is listening on localhost:${PORT}`)
+})
---
+++ routes/modules/home.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        res.render('index')
+    } catch (error) {
+        return next(error)
+    }
+})
+
+module.exports = router
