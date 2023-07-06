c28fbdeb50e31dddf45ab0834de4e768a36402a2
DannyHucc <dannyhu10011101@gmail.com>
2023-07-06 09:04:38 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,11 +4,14 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const User = require('models-file/user')
 
 // login
 router.get('/login', async (req, res, next) => {
     try {
-        res.render('users/login')
+        return res.render('users/login')
     } catch (error) {
         return next(error)
     }
@@ -22,7 +25,48 @@ router.post('/login', passport.authenticate('local', {
 // register
 router.get('/register', async (req, res, next) => {
     try {
-        res.render('users/register')
+        return res.render('users/register')
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const { name, email, password, confirmPassword } = req.body
+        const isUserExisted = await User.exists({ email })
+        const errors = []
+        const passwordRegex =
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()_+-={}\[\]\\:;"'<>?,.\/])[A-Za-z\d~!@#$%^&*()_+-={}\[\]\\:;"'<>?,.\/]{8,20}$/
+
+        // check if the register info is valid
+        if (isUserExisted) {
+            errors.push({ message: 'User already exists!' })
+        }
+
+        if (!validator.isEmail(email)) {
+            errors.push({ message: 'Email address is invalid.' })
+        }
+
+        if (!passwordRegex.test(password)) {
+            errors.push({ message: 'The password must be 8-20 characters long, contain letters and numbers and symbols.' })
+        }
+
+        if (password !== confirmPassword) {
+            errors.push({ message: 'The password confirmation dose not match.' })
+        }
+
+        if (errors.length) {
+            return res.render('users/register', { errors, name, email, password, confirmPassword })
+        }
+
+        // create the user register information
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt, null)
+        const userName = email.slice(0, Number(email.indexOf("@")))
+        await User.create({ name: userName, email, password: hash })
+        req.flash('success_msg', 'Register successfully! Please login to your account.')
+        return res.redirect('/users/login')
     } catch (error) {
         return next(error)
     }
