22659a686e064ae2ca37e46aac6202c3e8fe2993
DannyHucc <dannyhu10011101@gmail.com>
2023-07-08 09:35:38 +0800

    feat: add filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,30 +7,37 @@ const Record = require('models-file/record')
 
 router.get('/', async (req, res, next) => {
     try {
+        // get website data and set variable
         const userId = req.user._id
         let income = 0
         let expense = 0
 
+        // get database data
         const recordsData = await Record.find({ userId }).lean()
         const categories = await Category.find().lean()
 
+        // formate record data
         const records = await Promise.all(recordsData.map(async (record) => {
             if (record.type === 'income') income += record.amount
             if (record.type === 'expense') expense += record.amount
+
+            record.amount = new Intl.NumberFormat('en-US').format(record.amount)
             record.date = new Date(record.date).toISOString().slice(0, 10)
+
             const icon = categories.find((category) =>
                 category._id.toString() === record.categoryId.toString()).icon
             const formattedRecord = { ...record, icon }
             return formattedRecord
         }))
 
+        // calculate amount
         let totalAmount = income - expense
         const totalAmountColor = totalAmount >= 0 ? "success" : "danger"
-
         income = new Intl.NumberFormat('en-US').format(income)
         expense = new Intl.NumberFormat('en-US').format(expense)
         totalAmount = new Intl.NumberFormat('en-US').format(totalAmount)
 
+        // render page
         return res.render('index', {
             categories,
             records,
@@ -38,6 +45,65 @@ router.get('/', async (req, res, next) => {
             expense,
             totalAmount,
             totalAmountColor,
+            home: 'home',
+            javascript: ['index.js']
+        })
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.get('/filter', async (req, res, next) => {
+    try {
+        // get website data and set variable
+        const { sortCategory, sortMethod, sortTitle } = req.query
+        const userId = req.user._id
+        let income = 0
+        let expense = 0
+        const sort = {}
+        sort[sortTitle] = sortMethod
+
+        // get database data
+        const categories = await Category.find().lean()
+        const recordsData = sortCategory
+            ? await Record.find({ userId, categoryId: sortCategory }).sort(sort).lean()
+            : await Record.find({ userId }).sort(sort).lean()
+
+        // formate record data
+        const records = await Promise.all(recordsData.map((record) => {
+            if (record.type === 'income') income += record.amount
+            if (record.type === 'expense') expense += record.amount
+
+            record.amount = new Intl.NumberFormat('en-US').format(record.amount)
+            record.date = new Date(record.date).toISOString().slice(0, 10)
+
+            const icon = categories.find((category) =>
+                category._id.toString() === record.categoryId.toString()).icon
+            const formattedRecord = { ...record, icon }
+            return formattedRecord
+        }))
+
+        // calculate amount
+        let totalAmount = income - expense
+        const totalAmountColor = totalAmount >= 0 ? "success" : "danger"
+        income = new Intl.NumberFormat('en-US').format(income)
+        expense = new Intl.NumberFormat('en-US').format(expense)
+        totalAmount = new Intl.NumberFormat('en-US').format(totalAmount)
+
+        // render page
+        return res.render('index', {
+            categories,
+            records,
+            income,
+            expense,
+            totalAmount,
+            totalAmountColor,
+            sortCategory: sortCategory
+                ? categories.find((category) =>
+                    category._id.toString() === sortCategory).name
+                : "",
+            sortMethod,
+            sortTitle,
             home: 'home',
             javascript: ['index.js']
         })
